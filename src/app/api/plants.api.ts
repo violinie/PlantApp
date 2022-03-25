4cbba0d9a600b004d2f304ac7893faf66aca7a32
Hana Čopáková <hancacopakova@gmail.com>
2022-03-25 14:51:13 +0100

    feat(api): update data

--- src/app/api/plants.api.ts
+++ src/app/api/plants.api.ts
@@ -1,6 +1,13 @@
 import { Plant } from "../interfaces/plant.interface";
 
 export const plants: Plant[] = [
-  {id: 1, name: "Monstera", nameLatin: "Lorem", desctiption: "Lorem ipsum", image: "https://source.unsplash.com/random/280x280?plant", slug: "monstera"},
-  {id: 2, name: "Kaktus", nameLatin: "Lorem ipsum", desctiption: "Lorem ipsum", image: "https://source.unsplash.com/random/280x280?cactus", slug: "kaktus"}
+  {id: 1, name: "Monstera Deliciosa", desctiption: "Lorem ipsum", image: "https://source.unsplash.com/random/280x280?monstera", slug: "monstera"},
+  {id: 2, name: "Alocasia wentii", desctiption: "Lorem ipsum", image: "https://source.unsplash.com/random/280x280?alocasia", slug: "alocasia-wentii"},
+  {id: 3, name: "Philodendron scandens", desctiption: "Lorem ipsum", image: "https://source.unsplash.com/random/280x280?philodendron", slug: "philodendron-scandens"},
+  {id: 4, name: "Epipremnum Golden", desctiption: "Lorem ipsum", image: "https://source.unsplash.com/random/280x280?epipremnum", slug: "epipremnum-golden"},
+  {id: 5, name: "Scindapsus pictus Exotica", desctiption: "Lorem ipsum", image: "https://source.unsplash.com/random/280x280?scindapsus", slug: "scindapsus-pictus-exotica"},
+  {id: 6, name: "Syngonium Pixie", desctiption: "Lorem ipsum", image: "https://source.unsplash.com/random/280x280?syngonium", slug: "syngonium-pixie"},
+  {id: 7, name: "Zamioculcas zamiifolia", desctiption: "Lorem ipsum", image: "https://source.unsplash.com/random/280x280?zamioculcas", slug: "zamioculcas-zamiifolia"},
+  {id: 8, name: "Calathea orbifolia", desctiption: "Lorem ipsum", image: "https://source.unsplash.com/random/280x280?calathea", slug: "calathea-orbifolia"},
+  {id: 9, name: "Marantha leuconeura Fascinator", desctiption: "Lorem ipsum", image: "https://source.unsplash.com/random/280x280?marantha", slug: "marantha-leuconeura-fascinator"}
   ]
--- src/app/interfaces/plant.interface.ts
+++ src/app/interfaces/plant.interface.ts
@@ -1,7 +1,6 @@
 export interface Plant {
   id: number;
   name: string;
-  nameLatin: string;
   desctiption: string;
   image: string;
   slug: string;
