467bf2e1c079f6ef07c058357e90df8f4b8ded4b
Hana Čopáková <hancacopakova@gmail.com>
2022-03-25 15:30:18 +0100

    feat(card-detail): add data to component

--- src/app/api/plants.api.ts
+++ src/app/api/plants.api.ts
@@ -1,13 +1,13 @@
 import { Plant } from "../interfaces/plant.interface";
 
 export const plants: Plant[] = [
-  {id: 1, name: "Monstera Deliciosa", desctiption: "Lorem ipsum", image: "https://source.unsplash.com/random/280x280?monstera", slug: "monstera"},
-  {id: 2, name: "Alocasia wentii", desctiption: "Lorem ipsum", image: "https://source.unsplash.com/random/280x280?alocasia", slug: "alocasia-wentii"},
-  {id: 3, name: "Philodendron scandens", desctiption: "Lorem ipsum", image: "https://source.unsplash.com/random/280x280?philodendron", slug: "philodendron-scandens"},
-  {id: 4, name: "Epipremnum Golden", desctiption: "Lorem ipsum", image: "https://source.unsplash.com/random/280x280?epipremnum", slug: "epipremnum-golden"},
-  {id: 5, name: "Scindapsus pictus Exotica", desctiption: "Lorem ipsum", image: "https://source.unsplash.com/random/280x280?scindapsus", slug: "scindapsus-pictus-exotica"},
-  {id: 6, name: "Syngonium Pixie", desctiption: "Lorem ipsum", image: "https://source.unsplash.com/random/280x280?syngonium", slug: "syngonium-pixie"},
-  {id: 7, name: "Zamioculcas zamiifolia", desctiption: "Lorem ipsum", image: "https://source.unsplash.com/random/280x280?zamioculcas", slug: "zamioculcas-zamiifolia"},
-  {id: 8, name: "Calathea orbifolia", desctiption: "Lorem ipsum", image: "https://source.unsplash.com/random/280x280?calathea", slug: "calathea-orbifolia"},
-  {id: 9, name: "Marantha leuconeura Fascinator", desctiption: "Lorem ipsum", image: "https://source.unsplash.com/random/280x280?marantha", slug: "marantha-leuconeura-fascinator"}
+  {id: 1, name: "Monstera Deliciosa", desctiption: "Lorem ipsum", image: "https://source.unsplash.com/random/500x500?monstera", slug: "monstera"},
+  {id: 2, name: "Alocasia wentii", desctiption: "Lorem ipsum", image: "https://source.unsplash.com/random/500x500?alocasia", slug: "alocasia-wentii"},
+  {id: 3, name: "Philodendron scandens", desctiption: "Lorem ipsum", image: "https://source.unsplash.com/random/500x500?philodendron", slug: "philodendron-scandens"},
+  {id: 4, name: "Epipremnum Golden", desctiption: "Lorem ipsum", image: "https://source.unsplash.com/random/500x500?epipremnum", slug: "epipremnum-golden"},
+  {id: 5, name: "Scindapsus pictus Exotica", desctiption: "Lorem ipsum", image: "https://source.unsplash.com/random/500x500?scindapsus", slug: "scindapsus-pictus-exotica"},
+  {id: 6, name: "Syngonium Pixie", desctiption: "Lorem ipsum", image: "https://source.unsplash.com/random/500x500?syngonium", slug: "syngonium-pixie"},
+  {id: 7, name: "Zamioculcas zamiifolia", desctiption: "Lorem ipsum", image: "https://source.unsplash.com/random/500x500?zamioculcas", slug: "zamioculcas-zamiifolia"},
+  {id: 8, name: "Calathea orbifolia", desctiption: "Lorem ipsum", image: "https://source.unsplash.com/random/500x500?calathea", slug: "calathea-orbifolia"},
+  {id: 9, name: "Marantha leuconeura Fascinator", desctiption: "Lorem ipsum", image: "https://source.unsplash.com/random/500x500?marantha", slug: "marantha-leuconeura-fascinator"}
   ]
