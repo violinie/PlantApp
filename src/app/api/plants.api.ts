1f8492736e5bf012cdbb9fa4b22a92c22387c6c2
Hana Čopáková <hancacopakova@gmail.com>
2022-03-25 14:41:24 +0100

    feat(card): style

--- src/app/api/plants.api.ts
+++ src/app/api/plants.api.ts
@@ -1,6 +1,6 @@
 import { Plant } from "../interfaces/plant.interface";
 
 export const plants: Plant[] = [
-  {id: 1, name: "Monstera", nameLatin: "Lorem", desctiption: "Lorem ipsum", image: "https://source.unsplash.com/random/240?plant", slug: "monstera"},
-  {id: 2, name: "Kaktus", nameLatin: "Lorem ipsum", desctiption: "Lorem ipsum", image: "https://source.unsplash.com/random/240?cactus", slug: "kaktus"}
+  {id: 1, name: "Monstera", nameLatin: "Lorem", desctiption: "Lorem ipsum", image: "https://source.unsplash.com/random/280x280?plant", slug: "monstera"},
+  {id: 2, name: "Kaktus", nameLatin: "Lorem ipsum", desctiption: "Lorem ipsum", image: "https://source.unsplash.com/random/280x280?cactus", slug: "kaktus"}
   ]
