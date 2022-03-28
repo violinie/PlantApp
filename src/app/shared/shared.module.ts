c4526713a36e0cfff193fb78136b5a1874ac4335
Hana Čopáková <hancacopakova@gmail.com>
2022-03-28 13:24:57 +0200

    feat(shared): fontawesome init

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,19 +1,12 @@
-import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { CardComponent } from './components/card/card.component';
+import { NgModule } from '@angular/core';
 import { RouterModule } from '@angular/router';
-
-
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { CardComponent } from './components/card/card.component';
 
 @NgModule({
-  declarations: [
-    CardComponent
-  ],
-  imports: [
-    CommonModule, RouterModule
-  ],
-  exports: [
-    CardComponent
-  ]
+  declarations: [CardComponent],
+  imports: [CommonModule, RouterModule, FontAwesomeModule],
+  exports: [CardComponent, FontAwesomeModule]
 })
-export class SharedModule { }
+export class SharedModule {}
