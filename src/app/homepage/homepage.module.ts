471e20cad9f80025021e8aae66d0bdeb46c7ecb3
Hana Čopáková <hancacopakova@gmail.com>
2022-03-28 16:56:27 +0200

    feat(filter): create filter component

--- src/app/homepage/homepage.module.ts
+++ src/app/homepage/homepage.module.ts
@@ -3,9 +3,10 @@ import { NgModule } from '@angular/core';
 import { SharedModule } from '../shared/shared.module';
 import { HomepageRoutingModule } from './homepage-routing.module';
 import { HomepageComponent } from './pages/homepage/homepage.component';
+import { FilterComponent } from './components/filter/filter.component';
 
 @NgModule({
-  declarations: [HomepageComponent],
+  declarations: [HomepageComponent, FilterComponent],
   imports: [CommonModule, HomepageRoutingModule, SharedModule]
 })
 export class HomepageModule {}
