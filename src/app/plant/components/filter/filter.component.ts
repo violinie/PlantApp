0625b797e251947d8c181e457e929fc573b2e3a7
Hana Čopáková <hancacopakova@gmail.com>
2022-03-31 11:26:46 +0200

    fix(filter): wip

--- src/app/plant/components/filter/filter.component.ts
+++ src/app/plant/components/filter/filter.component.ts
@@ -1,5 +1,15 @@
 import { ChangeDetectionStrategy, Component, OnInit } from '@angular/core';
-import { Observable } from 'rxjs';
+import {
+  catchError,
+  concat,
+  distinctUntilChanged,
+  Observable,
+  of,
+  Subject,
+  switchMap,
+  tap
+} from 'rxjs';
+import { Plant } from 'src/app/interfaces/plant.interface';
 import { DataService } from './data.service';
 
 @Component({
@@ -9,20 +19,49 @@ import { DataService } from './data.service';
   changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class FilterComponent implements OnInit {
-  people$!: Observable<any[]>;
-  selectedPeople = [{ name: 'Karyn Wright' }];
+  public plants$!: Observable<any[]>;
+  public plants: any[] = [];
+  selectedPlants = [{ species: 'Všechny' }];
 
-  constructor(private dataService: DataService) {}
+  constructor(private filterService: DataService) {}
 
   ngOnInit() {
-    this.people$ = this.dataService.getPeople();
+    this.filterService.getPlants().subscribe(x => {
+      console.log('filterrr', x);
+      this.plants = x;
+    });
+    this.loadPlants();
   }
 
   clearModel() {
-    this.selectedPeople = [];
+    this.selectedPlants = [];
   }
 
   changeModel() {
-    this.selectedPeople = [{ name: 'New person' }];
+    this.selectedPlants = [{ species: 'New Plant' }];
+  }
+
+  people$!: Observable<Plant[]>;
+  peopleLoading = false;
+  peopleInput$ = new Subject<string>();
+
+  trackByFn(item: Plant) {
+    return item.id;
+  }
+
+  private loadPlants() {
+    this.people$ = concat(
+      of([]), // default items
+      this.peopleInput$.pipe(
+        distinctUntilChanged(),
+        tap(() => (this.peopleLoading = true)),
+        switchMap(term =>
+          this.filterService.getPlants(term).pipe(
+            catchError(() => of([])), // empty list on error
+            tap(() => (this.peopleLoading = false))
+          )
+        )
+      )
+    );
   }
 }
