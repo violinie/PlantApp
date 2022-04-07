8477329f6833e0d4974c8c3d1e8ce2754a474eb0
Hana Čopáková <hancacopakova@gmail.com>
2022-04-07 13:44:08 +0200

    feat(plant): fix img size & ratio

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -3,10 +3,11 @@ import { NgModule } from '@angular/core';
 import { RouterModule } from '@angular/router';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 import { CardComponent } from './components/card/card.component';
+import { PlaceholderComponent } from './components/placeholder/placeholder.component';
 
 @NgModule({
-  declarations: [CardComponent],
+  declarations: [CardComponent, PlaceholderComponent],
   imports: [CommonModule, RouterModule, FontAwesomeModule],
-  exports: [CardComponent, FontAwesomeModule]
+  exports: [CardComponent, FontAwesomeModule, PlaceholderComponent]
 })
 export class SharedModule {}
