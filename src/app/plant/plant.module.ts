f63829ee557902f746a4538e487ed5e1c2521da2
Hana Čopáková <hancacopakova@gmail.com>
2022-04-13 10:17:41 +0200

    ref(tags): folder name change

--- src/app/plant/plant.module.ts
+++ src/app/plant/plant.module.ts
@@ -5,7 +5,7 @@ import { NgSelectModule } from '@ng-select/ng-select';
 import { SharedModule } from '../shared/shared.module';
 import { BreadcrumbsComponent } from './components/breadcrumbs/breadcrumbs.component';
 import { FilterComponent } from './components/filter/filter.component';
-import { TagsComponent } from './components/filtered/tags.component';
+import { TagsComponent } from './components/tags/tags.component';
 import { DetailComponent } from './pages/detail/detail.component';
 import { PlantRoutingModule } from './plant-routing.module';
 import { PlantComponent } from './plant.component';
